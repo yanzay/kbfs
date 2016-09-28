f73e480f9d2f272353df6ffcde517d48461baae7
Jeremy Stribling <strib@alum.mit.edu>
2016-09-28 12:14:33 -0700

    data_types: add a revision to writerInfo
    Issue: KBFS-1473

--- libkbfs/conflict_resolver.go
+++ libkbfs/conflict_resolver.go
@@ -1058,7 +1058,9 @@ func (cr *ConflictResolver) addRecreateOpsToUnmergedChains(ctx context.Context,
 	if err != nil {
 		return nil, err
 	}
-	winfo, err := newWriterInfo(ctx, cr.config, uid, unmergedChains.mostRecentMD.LastModifyingWriterKID())
+	winfo, err := newWriterInfo(ctx, cr.config, uid,
+		unmergedChains.mostRecentMD.LastModifyingWriterKID(),
+		unmergedChains.mostRecentMD.Revision())
 	if err != nil {
 		return nil, err
 	}
@@ -1601,7 +1603,8 @@ func (cr *ConflictResolver) addMergedRecreates(ctx context.Context,
 					co.AddRefBlock(c.mostRecent)
 					winfo, err := newWriterInfo(ctx, cr.config,
 						mergedChains.mostRecentMD.LastModifyingWriter(),
-						mergedChains.mostRecentMD.LastModifyingWriterKID())
+						mergedChains.mostRecentMD.LastModifyingWriterKID(),
+						mergedChains.mostRecentMD.Revision())
 					if err != nil {
 						return err
 					}
